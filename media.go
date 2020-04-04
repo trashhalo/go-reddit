d66c33e1393892eaa738ddc455d3a82265db772d
Stephen Solka <s@0qz.fun>
2020-04-04 16:53:07 -0400

    issues with oembed

--- media.go
+++ media.go
@@ -8,16 +8,16 @@ type Media struct {
 
 // Oembed contains embedding information for a media item.
 type Oembed struct {
-	Description     string `json:"description"`
-	HTML            string `json:"html"`
-	Height          string `json:"height"`
-	ProviderName    string `json:"provider_name"`
-	ProviderURL     string `json:"provider_url"`
-	ThumbnailHeight int    `json:"thumbnail_height"`
-	ThumbnailURL    string `json:"thumbnail_url"`
-	ThumbnailWidth  int    `json:"thumbnail_width"`
-	Title           string `json:"title"`
-	Type            string `json:"type"`
-	Version         string `json:"version"`
-	Width           int    `json:"width"`
+	Description     string      `json:"description"`
+	HTML            string      `json:"html"`
+	Height          interface{} `json:"height"`
+	ProviderName    string      `json:"provider_name"`
+	ProviderURL     string      `json:"provider_url"`
+	ThumbnailHeight int         `json:"thumbnail_height"`
+	ThumbnailURL    string      `json:"thumbnail_url"`
+	ThumbnailWidth  int         `json:"thumbnail_width"`
+	Title           string      `json:"title"`
+	Type            string      `json:"type"`
+	Version         string      `json:"version"`
+	Width           interface{} `json:"width"`
 }
